6206243c98d40dc56933f8f94bec819ae20f2fb1
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-10-09 14:54:10 +0300

    css

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+$(".cubatheme").mouseover(function () {
+    $("#cubatext").show();
+});
+$(".cubatheme").mouseleave(function () {
+    $("#cubatext").hide();
+});
+
+$(".chinatheme").mouseover(function () {
+    $("#chinatext").show();
+});
+$(".chinatheme").mouseleave(function() {
+    $("#chinatext").hide();
+});
+
+$(".paristheme").mouseover(function() {
+    $("#paristext").show();
+});
+$(".paristheme").mouseleave(function() {
+    $("#paristext").hide();
+});
+
+$(".indiatheme").mouseover(function() {
+    $("#indiatext").show();
+});
+$(".indiatheme").mouseleave(function (){
+    $("#indiatext").hide();
+});
+
+$(".egypttheme").mouseover(function(){
+    $("#egypttheme").show();
+});
+$(".egypttheme").mouseleave(function(){
+    $("#egypttheme").hide();
+});
+
+$(".japantheme").mouseover(function() {
+    $("#japantext").show();
+});
+$(".japantheme").mouseleave(function() {
+    $("#japantext").hide();
+});
+})
