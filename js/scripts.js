4aad8413936772b66c93e184a497b04245b7fd60
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-10-09 16:15:02 +0300

    CSS & hover

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-$(".cubatheme").mouseover(function () {
-    $("#cubatext").show();
-});
-$(".cubatheme").mouseleave(function () {
-    $("#cubatext").hide();
-});
+    $(".cubatheme").mouseover(function () {
+        $("#cubatext").show();
+    });
+    $(".cubatheme").mouseleave(function () {
+        $("#cubatext").hide();
+    });
 
-$(".chinatheme").mouseover(function () {
-    $("#chinatext").show();
-});
-$(".chinatheme").mouseleave(function() {
-    $("#chinatext").hide();
-});
+    $(".chinatheme").mouseover(function () {
+        $("#chinatext").show();
+    });
+    $(".chinatheme").mouseleave(function () {
+        $("#chinatext").hide();
+    });
 
-$(".paristheme").mouseover(function() {
-    $("#paristext").show();
-});
-$(".paristheme").mouseleave(function() {
-    $("#paristext").hide();
-});
+    $(".paristheme").mouseover(function () {
+        $("#paristext").show();
+    });
+    $(".paristheme").mouseleave(function () {
+        $("#paristext").hide();
+    });
 
-$(".indiatheme").mouseover(function() {
-    $("#indiatext").show();
-});
-$(".indiatheme").mouseleave(function (){
-    $("#indiatext").hide();
-});
+    $(".indiatheme").mouseover(function () {
+        $("#indiatext").show();
+    });
+    $(".indiatheme").mouseleave(function () {
+        $("#indiatext").hide();
+    });
 
-$(".egypttheme").mouseover(function(){
-    $("#egypttheme").show();
-});
-$(".egypttheme").mouseleave(function(){
-    $("#egypttheme").hide();
-});
+    $(".egypttheme").mouseover(function () {
+        $("#egypttext").show();
+    });
+    $(".egypttheme").mouseleave(function () {
+        $("#egypttext").hide();
+    });
 
-$(".japantheme").mouseover(function() {
-    $("#japantext").show();
-});
-$(".japantheme").mouseleave(function() {
-    $("#japantext").hide();
-});
+    $(".japantheme").mouseover(function () {
+        $("#japantext").show();
+    });
+    $(".japantheme").mouseleave(function () {
+        $("#japantext").hide();
+    });
 })
