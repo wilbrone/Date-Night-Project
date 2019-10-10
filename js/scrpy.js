af4db1d76eba62fe514198e6ea5159eb35289ea8
wilbrone <wilbroneokoth@gmail.com>
2019-10-11 02:39:20 +0300

    add display for new users

--- js/scrpy.js
+++ js/scrpy.js
@@ -1,19 +1,58 @@
 $(document).ready(function(){
     if (window.localStorage && localStorage.user_details) {
-        var fontSize = JSON.parse(localStorage.getItem("user_details"));
+        var myArray = JSON.parse(localStorage.getItem("user_details"));
+
+        for(i=0; i<myArray.length; i++){
+        var user = myArray[i];
+
+        }
+        console.log(user);
+
+        $(".output").text("Welcome, " + user.user_Name.toUpperCase());
+
+        var letter = user.user_Name[0].toUpperCase();
+        console.log(letter);
+        $("#profileImage").text(letter);
+
+    }else if (window.localStorage && localStorage.new_user) {
+        var myArray = JSON.parse(localStorage.getItem("new_user"));
+
+        for(i=0; i<myArray.length; i++){
+        var user = myArray[i];
+
+        }
+        console.log(user);
+
+        $(".output").text("Welcome, " + user.uName.toUpperCase());
+
+        var letter = user.uName[0].toUpperCase();
+        console.log(letter);
+        $("#profileImage").text(letter);
+
     };
-    console.log(fontSize);
+
+    $("form#sign-up").submit(function(){
+        if (window.localStorage && localStorage.new_user) {
+            var myArray = JSON.parse(localStorage.getItem("new_user"));
+
+            for(i=0; i<myArray.length; i++){
+            var user = myArray[i];
+
+            }
+            console.log(user);
+
+            $(".output").text("Welcome, " + user.uName.toUpperCase());
+
+            var letter = user.uName[0].toUpperCase();
+            console.log(letter);
+            $("#profileImage").text(letter);
+
+        };
+    });
 
 
-    for(i=0; i<fontSize.length; i++){
-        var user = fontSize[i];
-
-    }
-    console.log(user);
-
-    $(".output").text("Welcome, " + user.user_Name.toUpperCase());
-
-    var letter = user.user_Name[0].toUpperCase();
-    console.log(letter);
-    $("#profileImage").text(letter);
+    $("#logout").click(function(){
+       localStorage.clear();
+       window.location="landing-page.html"; 
+    });
 });
