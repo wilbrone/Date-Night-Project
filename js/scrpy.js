ed072a900cd72f191d7aa30fc7e0b1ff8b58ec3d
wilbrone <wilbroneokoth@gmail.com>
2019-10-11 00:49:46 +0300

    styling

--- js/scrpy.js
+++ js/scrpy.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     if (window.localStorage && localStorage.user_details) {
         fontSize = localStorage.getItem("user_details");
         console.log(fontSize)
-        $(".output").textContent = "The font size has carried over pages, and is: " + fontSize;
+        // $(".output").textContent = "The font size has carried over pages, and is: " + fontSize;
             // document.getElementById("output").style.fontSize = fontSize;
     }
 });
