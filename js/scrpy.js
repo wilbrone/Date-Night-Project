ebe4b2f7fddb413ec2d5ce5a2317245cfb6e2e32
wilbrone <wilbroneokoth@gmail.com>
2019-10-11 02:53:10 +0300

    code clean up

--- js/scrpy.js
+++ js/scrpy.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 
         var letter = user.user_Name[0].toUpperCase();
         console.log(letter);
+        
         $("#profileImage").text(letter);
 
     }else if (window.localStorage && localStorage.new_user) {
@@ -31,26 +32,8 @@ $(document).ready(function(){
 
     };
 
-    $("form#sign-up").submit(function(){
-        if (window.localStorage && localStorage.new_user) {
-            var myArray = JSON.parse(localStorage.getItem("new_user"));
 
-            for(i=0; i<myArray.length; i++){
-            var user = myArray[i];
-
-            }
-            console.log(user);
-
-            $(".output").text("Welcome, " + user.uName.toUpperCase());
-
-            var letter = user.uName[0].toUpperCase();
-            console.log(letter);
-            $("#profileImage").text(letter);
-
-        };
-    });
-
-
+// CLEARS THE DATA FROM THE LOCAL STORAGE AND REDIRECTS TO LOGIN PAGE(LANDING-PAGE)
     $("#logout").click(function(){
        localStorage.clear();
        window.location="landing-page.html"; 
